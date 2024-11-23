80a42ed504c463c6c2adac90ee259a0657471cf8
Sander Buruma <sanderburuma@fastmail.nl>
2024-11-23 12:21:31 +0000

    use the factorio gear as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Factorio Galaxy Database",
   description: "Browse and search the Factorio Galaxy stars database",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
